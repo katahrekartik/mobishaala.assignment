815cdb33ebe146467269a9dae3a11870f5d87fb4
2016118@iiitdmj.ac.in <39498672+katahrekartik@users.noreply.github.com>
2020-06-24 15:44:26 +0530

    Updates

---
+++ precache-manifest.0c3b0af4e2586a38621fdc784ace6f84.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa484f11d1818bd0ea47295514614d6d",
+    "url": "/mobishaala.assignment/index.html"
+  },
+  {
+    "revision": "1f9e4bb42e1ed4a4b06f",
+    "url": "/mobishaala.assignment/static/css/2.81f5d3e0.chunk.css"
+  },
+  {
+    "revision": "a3439634226cab39f4f1",
+    "url": "/mobishaala.assignment/static/css/main.b189e472.chunk.css"
+  },
+  {
+    "revision": "1f9e4bb42e1ed4a4b06f",
+    "url": "/mobishaala.assignment/static/js/2.ca7824a8.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/mobishaala.assignment/static/js/2.ca7824a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3439634226cab39f4f1",
+    "url": "/mobishaala.assignment/static/js/main.1af24dbf.chunk.js"
+  },
+  {
+    "revision": "f65db689e0f230fc398b",
+    "url": "/mobishaala.assignment/static/js/runtime-main.0aebaede.js"
+  },
+  {
+    "revision": "a3a0c9d14b637b5d756428aa180a9016",
+    "url": "/mobishaala.assignment/static/media/education.a3a0c9d1.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mobishaala.assignment/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mobishaala.assignment/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mobishaala.assignment/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mobishaala.assignment/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mobishaala.assignment/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
